187467fe7990cb51995af19769cba4c5f777eff8
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2024-03-16 02:31:04 +0100

    created header, added logo, added homepage layout, and authentication

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,19 @@
+import { Footer } from './footer/page';
+import { Header } from './header/page';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MarketingLayout = ({ children }: Props) => {
+  return (
+    <div className='min-h-screen flex flex-col'>
+      <Header />
+      <main className='flex flex-1 flex-col items-center justify-center'>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+export default MarketingLayout;
