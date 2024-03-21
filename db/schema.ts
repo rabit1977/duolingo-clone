b11da9e68ba075af5bfda8c63ba5013514ff69ff
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2024-03-22 00:00:41 +0100

    fixed some naming otpions

--- db/schema.ts
+++ db/schema.ts
@@ -77,7 +77,7 @@ export const challengesRelations = relations(challenges, ({ one, many }) => ({
   challengeProgress: many(challengeProgress),
 }));
 
-export const challengeOptions = pgTable('challengeOptions', {
+export const challengeOptions = pgTable('challenge_options', {
   id: serial('id').primaryKey(),
   challengeId: integer('challenge_id')
     .references(() => challenges.id, { onDelete: 'cascade' })
@@ -98,7 +98,7 @@ export const challengeOptionsRelations = relations(
   })
 );
 
-export const challengeProgress = pgTable('challengeProgress', {
+export const challengeProgress = pgTable('challenge_progress', {
   id: serial('id').primaryKey(),
   userId: text('user_id').notNull(),
   challengeId: integer('challenge_id')
