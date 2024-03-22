f1bfb503337be37e21d469efd979fe6da20e7e61
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2024-03-22 22:39:45 +0100

    added more queries, schema.ts and seed.ts stuff

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { auth } from '@clerk/nextjs';
 import { eq } from 'drizzle-orm';
 import { cache } from 'react';
 import db from './drizzle';
-import { courses, userProgress } from './schema';
+import { courses, units, userProgress } from './schema';
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
@@ -24,6 +24,36 @@ export const getUserProgress = cache(async () => {
     },
   });
   return data;
+});
+
+export const getUnits = cache(async () => {
+  const progress = await getUserProgress();
+
+  if (!userProgress?.activeCourseId) {
+    return [];
+  }
+
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  const noramlizeData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return challenge.challengeProgress && challenge.challengeProgress.length > 0 && challenge.challengeProgress.every((progress) => progress.completed)
+      })
+    })
+  })
 });
 
 export const getCourseById = cache(async (courseId: number) => {
